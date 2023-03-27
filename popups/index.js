86104649ba8534f13a43fa6a7d517da0cffc5154
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2023-03-27 15:46:40 +0530

    Added Quality Selector

--- popups/index.js
+++ popups/index.js
@@ -11,18 +11,22 @@ let port = null;
 function sendToContent(tabs) {
     const url = tabs[0].url;
     const tabId = tabs[0].id;
-    const vidQ = '720p';
     // Long lived Connection
     if (port == null)
         port = chrome.tabs.connect(tabId);
 
     const connection = () => {
-        console.log('Sending message to content.js .......');
-        port.postMessage({ url, vidQ });
+        // console.log('Sending message to content.js .......');
+        port.postMessage(null);
         port.onMessage.addListener((res) => {
             // Response from content.js
             if (res) {
-                renderURL(res.downloadURL);
+                if (!res.downloadURL) {
+                    showQualityOptions(res, port);
+                }
+                else {
+                    renderURL(res.downloadURL);
+                }
             } else {
                 renderURL();
             }
@@ -31,17 +35,71 @@ function sendToContent(tabs) {
     connection();
 }
 
-function renderURL(url) {
+
+/*
+    data1.links.forAll.forAll[{f,q}]
+ */
+function showQualityOptions(res, port) {
+    // Show dropdown for quality
+    const data1 = res.data1;
+
+    const quality = document.getElementById('quality');
+    quality.innerHTML = '';
+
+    //mp4 
+    if (data1.links.mp4)
+        for (let key in data1.links['mp4']) {
+            let obj = data1.links['mp4'][key];
+            let f = obj.f;
+            let q = obj.q;
+            let opt = document.createElement('option');
+            opt.setAttribute('value', q + ',' + f);
+            opt.innerHTML = q;
+            quality.append(opt);
+        }
+    //ogg
+    if (data1.links.ogg)
+        for (let key in data1.links['ogg']) {
+            let obj = data1.links['ogg'][key];
+            let f = obj.f;
+            let q = obj.q;
+            let opt = document.createElement('option');
+            opt.setAttribute('value', q + ',' + f);
+            opt.innerHTML = q + ' ogg';
+            quality.append(opt);
+        }
+    //mp3
+    if (data1.links.mp3)
+        for (let key in data1.links['mp3']) {
+            let obj = data1.links['mp3'][key];
+            let f = obj.f;
+            let q = obj.q;
+            let opt = document.createElement('option');
+            opt.setAttribute('value', q + ',' + f);
+            opt.innerHTML = q + ' mp3';
+            quality.append(opt);
+        }
+
     const dnld = document.getElementById('dnld');
+    dnld.removeAttribute('disabled');
+    dnld.onclick = (event) => {
+        event.preventDefault();
+        const val = quality.value;
+        let vidQ = val.substring(0, val.indexOf(','));
+        let f = val.substring(val.indexOf(',') + 1);
+        // console.log(vidQ, f);
+        port.postMessage({ vidQ, f, data1 });
+    }
+}
+
+
+function renderURL(url) {
     const para = document.getElementById('para');
     if (url) {
         para.style.display = 'none';
-        dnld.removeAttribute('disabled');
-        dnld.onclick = (event) => {
-            event.preventDefault();
-            document.location.href = url;
-        }
+        document.location.href = url;
     } else {
         para.style.display = 'block';
     }
+    port = null;
 }
--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-function getLink({ vidQ, port }) {
+function firstRequest(port) {
     let videoURL = document.URL;
 
     // Request 1
@@ -10,65 +10,91 @@ function getLink({ vidQ, port }) {
     xhr1.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr1.send(params1);
     xhr1.onerror = () => {
-        // alert('Please try again');
         port.postMessage(null);
     }
     xhr1.onload = () => {
         const data1 = JSON.parse(xhr1.responseText);
-        let v_id = data1.vid;
-        let ftype = 'mp4';
-        let fquality = vidQ ? vidQ : Object.values(data1.links.mp4)[0].q;
-        let token = data1.token;
-        let timeExpire = data1.timeExpires;
-        const x_requested_key = 'de0cfuirtgf67a';
+        port.postMessage({ data1 });
+    }
+}
 
-        // Request 2
+function otherRequests(port, data1, vidQ, f) {
+    let v_id = data1.vid;
+    let ftype = f;
+    let fquality = vidQ ? vidQ : Object.values(data1.links.mp4)[0].q;
+    let token = data1.token;
+    let timeExpire = data1.timeExpires;
+    const x_requested_key = 'de0cfuirtgf67a';
 
-        const xhr2 = new XMLHttpRequest();
-        const url2 = 'https://backend.svcenter.xyz/api/convert-by-45fc4be8916916ba3b8d61dd6e0d6994';
-        const params2 = `v_id=${v_id}&ftype=${ftype}&fquality=${fquality}&token=${token}&timeExpire=${timeExpire}`;
-        xhr2.open('post', url2, true);
-        xhr2.setRequestHeader('x-requested-key', x_requested_key);
-        xhr2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr2.send(params2);
-        xhr2.onerror = () => {
-            // alert('Please try again');
+    // Request 2
+
+    const xhr2 = new XMLHttpRequest();
+    const url2 = 'https://backend.svcenter.xyz/api/convert-by-45fc4be8916916ba3b8d61dd6e0d6994';
+    const params2 = `v_id=${v_id}&ftype=${ftype}&fquality=${fquality}&token=${token}&timeExpire=${timeExpire}`;
+    xhr2.open('post', url2, true);
+    xhr2.setRequestHeader('x-requested-key', x_requested_key);
+    xhr2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr2.send(params2);
+    xhr2.onerror = () => {
+        port.postMessage(null);
+    }
+    xhr2.onload = () => {
+        let fname = data1.title ? data1.title : data1.vid;
+        const data2 = JSON.parse(xhr2.responseText);
+
+        // Request 3
+
+        const xhr3 = new XMLHttpRequest();
+        const c_server = data2.c_server;
+        const url3 = c_server + '/api/json/convert';
+        const params3 = params2 + `&fname=${fname}`;
+        xhr3.open('post', url3, true);
+        xhr3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr3.send(params3);
+        xhr3.onerror = () => {
             port.postMessage(null);
         }
-        xhr2.onload = () => {
-            let fname = data1.title ? data1.title : data1.vid;
-            const data2 = JSON.parse(xhr2.responseText);
+        xhr3.onload = () => {
+            const data3 = JSON.parse(xhr3.responseText);
 
-            // Request 3
-
-            const xhr3 = new XMLHttpRequest();
-            const url3 = data2.c_server + '/api/json/convert';
-            const params3 = params2 + `&fname=${fname}`;
-            xhr3.open('post', url3, true);
-            xhr3.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhr3.send(params3);
-            xhr3.onerror = () => {
-                // alert('Please try again');
-                port.postMessage(null);
-            }
-            xhr3.onload = () => {
-                const data3 = JSON.parse(xhr3.responseText);
-
-                // Send response to Popup
-                console.log(data3.result);
+            // Send response to Popup
+            if (data3.statusCode === 200) {
+                console.log('Video URL:', data3.result);
                 port.postMessage({ downloadURL: data3.result });
+            }
+            else if (data3.statusCode === 300) {
+                const jobId = data3.jobId;
+                const url4 = 'wss' + c_server.substring(c_server.indexOf(':')) + '/sub/' + jobId + '?fname=SaveTube.App';
+                const socket = new WebSocket(url4);
+                socket.onerror = () => {
+                    port.postMessage(null);
+                }
+                socket.onmessage = (res) => {
+                    const data4 = JSON.parse(res.data);
+                    if (data4.action == 'success') {
+                        console.log('Video URL:', data4.url);
+                        port.postMessage({ downloadURL: data4.url });
+                    } else {
+                        port.postMessage(null);
+                    }
+                }
+            } else {
+                port.postMessage(null);
             }
         }
     }
 }
-
 
 // Long-lived connection btw popup and content using port
 // Message from Popup
 chrome.runtime.onConnect.addListener((port) => {
     port.onMessage.addListener((msg) => {
         // console.log(msg);
-        const { url, vidQ } = msg;
-        getLink({ vidQ, port });
+        if (!msg) {
+            firstRequest(port);
+        } else {
+            const { vidQ, f, data1 } = msg;
+            otherRequests(port, data1, vidQ, f);
+        }
     });
 });
